55b2b4234e02a83044bdd9f05492ff70a2375786
yusronab <yusron.arly@gmail.com>
2023-08-21 07:02:46 +0700

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
+import { useEffect, useState } from "react";
 import { Route, Routes, useLocation } from "react-router-dom"
+import { FaClock } from "react-icons/fa";
 import Credential from "./pages/Credential";
 import NamePage from "./pages/NamePage";
 import Scanner from "./pages/Scanner";
@@ -12,6 +14,27 @@ const TodoPage = transition({ OgComponent: Todo });
 
 function App() {
   const location = useLocation();
+  const [isOpen, setIsOpen] = useState(false);
+
+  useEffect(() => {
+    const now = new Date().getHours();
+
+    if (now >= 9 && now < 17) {
+      setIsOpen(true);
+    } else {
+      setIsOpen(false);
+    }
+  }, []);
+
+  if (!isOpen) {
+    return (
+      <div className="w-screen h-screen flex justify-center items-center bg-gradient-to-l from-green-700
+      to-teal-700 flex-col gap-4 text-white">
+        <FaClock color="white" size={26} className="animate-bounce" />
+        <p>Presensi hanya bisa dibuka antara jam 9 dan 17.</p>
+      </div>
+    );
+  }
 
   return (
     <div className="w-screen h-screen flex justify-center items-center bg-gradient-to-l from-green-700
